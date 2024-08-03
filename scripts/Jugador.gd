9783e385015bc01e44a19f00e19cf4c73eaf12be
flendoh <flenditomlgcreations@gmail.com>
2024-08-03 10:46:39 -0500

    update jugador

--- scripts/Jugador.gd
+++ scripts/Jugador.gd
@@ -6,36 +6,40 @@ const SALTAR := 300.0
 
 var direccion := 0.0
 
+var vida = 100
+
 enum Modo {
-	TANQUE,
+	FLECHERO,
 	MAGO,
-	FLECHERO
+	TANQUE,
+	
 }
 
 
 #@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-#@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision = $CollisionShape2D
 
-var current_mode := Modo.TANQUE
+var current_mode := Modo.FLECHERO
 
 var modos := {
 	Modo.TANQUE: {
 		"idle": "tanque_idle",
 		"walk": "tanque_walk",
 		"habilidad": "tanque_habilidad",
-		#"sprite": preload("res://sprites/tanque.png")
+		"sprite": preload("res://assets/jugador/tanque.jpg")
 	},
 	Modo.MAGO: {
 		"idle": "mago_idle",
 		"walk": "mago_walk",
 		"habilidad": "mago_habilidad",
-		#"sprite": preload("res://sprites/mago.png")
+		"sprite": preload("res://assets/jugador/mago.jpg")
 	},
 	Modo.FLECHERO: {
 		"idle": "flechero_idle",
 		"walk": "flechero_walk",
 		"habilidad": "flechero_habilidad",
-		#"sprite": preload("res://sprites/flechero.png")
+		"sprite": preload("res://assets/jugador/flechero.jpg")
 	}
 }
 
@@ -50,24 +54,36 @@ func _physics_process(delta):
 		velocity.y -= SALTAR
 		
 	if !is_on_floor():
-		velocity.y += GRAVEDAD 
+		velocity.y += GRAVEDAD
 
 	move_and_slide()
 
 
-func cambiar_modo():
-	current_mode = (current_mode + 1) % Modo.size()
+func cambiar_modo(op):
+	current_mode = op
+	match op:
+		0:
+			collision.scale.y = 1.5
+		1:
+			collision.scale.y = 1
+		2:
+			collision.scale.y = 1
+
 	actualizar_modo()
 
 func actualizar_modo():
 	var modo_actual = modos[current_mode]
 	#animated_sprite.animation = modo_actual["idle"]
-	#sprite.texture = modo_actual["sprite"]
+	sprite.texture = modo_actual["sprite"]
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_T:
-			cambiar_modo()
+		if event.keycode == KEY_1:
+			cambiar_modo(0)
+		elif event.keycode == KEY_2:
+			cambiar_modo(1)
+		elif event.keycode == KEY_3:
+			cambiar_modo(2)
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			usar_habilidad()
