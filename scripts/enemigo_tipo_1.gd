fecf0671d36e8a60993cfac38ea9abad379b47da
flendoh <flenditomlgcreations@gmail.com>
2024-08-04 17:10:17 -0500

    detallessssss

--- scripts/enemigo_tipo_1.gd
+++ scripts/enemigo_tipo_1.gd
@@ -1,15 +1,8 @@
 extends CharacterBody2D
 
-<<<<<<< HEAD
-
-var SPEED = 200
-const JUMP_VELOCITY = -400.0
-var range = 100
-=======
 var vida = 3
 var SPEED = 40
 var range = 400
->>>>>>> ea2e24ca3107a0e8c497086c69c6dea2acdd3070
 var original_position
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,20 +13,11 @@ func _ready():
 	original_position = position.x
 
 func movimiento(delta):
-<<<<<<< HEAD
-=======
 	
->>>>>>> ea2e24ca3107a0e8c497086c69c6dea2acdd3070
 	if position.x > original_position+range:
 		SPEED = SPEED*-1
 	elif position.x < original_position-range:
 		SPEED = SPEED*-1
-<<<<<<< HEAD
-	position.x += SPEED*delta
-	
-func _process(delta):
-	movimiento(delta)
-=======
 			
 	if SPEED < 0 : 
 		$Sprite2D.flip_h = 0
@@ -62,4 +46,3 @@ func dead():
 	await $AnimationPlayer.animation_finished
 	queue_free()
 
->>>>>>> ea2e24ca3107a0e8c497086c69c6dea2acdd3070
